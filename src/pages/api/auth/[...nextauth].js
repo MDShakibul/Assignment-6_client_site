32f0e73ffac897666681d80576cc64dc8e813355
MDShakibul <aitldev14@gmail.com>
2023-07-30 14:56:01 +0600

    change page for product card

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -13,7 +13,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
   ],
   pages: {
     signIn: "/login"
